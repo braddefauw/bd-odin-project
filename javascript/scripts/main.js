eb571de4f5275e2eff0f7107f511f58bbd276ddd
braddefauw <badefauw@gmail.com>
2022-04-08 09:39:37 -0500

    OBJECTS AND OBJECT CONSTRUCTORS practice

--- javascript/scripts/main.js
+++ javascript/scripts/main.js
@@ -32,3 +32,57 @@ if(!localStorage.getItem('name')) {
 myButton.onclick = function() {
     setUserName();
 }
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read;
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+
+// console.log(theHobbit.info());
+
+// function printStuff(myDocuments) {
+//     this.documents = myDocuments;
+// }
+
+// printStuff.prototype.print = function(){
+//     console.log(this.documents);
+// }
+
+// var newObj = new printStuff("I am a new Object and I can print.");
+
+// newObj.print();
+
+function Student() {
+}
+
+Student.prototype.sayName = function() {
+  console.log(this.name)
+}
+
+function EighthGrader(name) {
+  this.name = name
+  this.grade = 8
+}
+
+// don't do this!!!
+EighthGrader.prototype = Student.prototype
+
+function NinthGrader(name) {
+  this.name = name
+  this.grade = 9
+}
+
+// noooo! not again!
+NinthGrader.prototype = Student.prototype
+
+NinthGrader.prototype.sayName = function() {console.log("HAHAHAHAHAHA")}
+
+const carl = new EighthGrader("carl")
+carl.sayName() //uh oh! this logs "HAHAHAHAHAHA" because we edited the sayName function!
