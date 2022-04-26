80e18f8ebb04896ce702bc8239c63d3412294f91
braddefauw <badefauw@gmail.com>
2022-04-26 10:22:55 -0500

    import/export practice

--- javascript/webpack-demo/src/index.js
+++ javascript/webpack-demo/src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    
+    element.textContent = myName('Brad');
     return element;
   }
   
