5a2baa2597e016943d5b28d8f18d1093c279a763
braddefauw <badefauw@gmail.com>
2022-04-27 10:00:22 -0500

    asset management practice

--- javascript/webpack-demo/src/index.js
+++ javascript/webpack-demo/src/index.js
@@ -1,13 +1,12 @@
 import _ from 'lodash';
-import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     
     element.textContent = myName('Brad');
+
     return element;
   }
   
