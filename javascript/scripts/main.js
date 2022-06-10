e614f5c3c81449d8e7e47255ea409e66529237a7
Brad DeFauw <badefauw@gmail.com>
2022-06-10 08:59:25 -0500

    installed prettier

--- javascript/scripts/main.js
+++ javascript/scripts/main.js
@@ -1,37 +1,37 @@
-let myImage = document.querySelector('img');
+let myImage = document.querySelector("img");
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute('src','images/firefox-icon.png');
-    }
-}
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
 function setUserName() {
-    let myName = prompt('Please enter your name.');
-    if(!myName) {
-        setUserName();
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.textContent = 'Mozilla is cool, ' + myName;
-    }
-  }
-
-if(!localStorage.getItem('name')) {
+  let myName = prompt("Please enter your name.");
+  if (!myName) {
     setUserName();
   } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla is cool, " + myName;
+  }
 }
 
-myButton.onclick = function() {
-    setUserName();
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is cool, " + storedName;
 }
+
+myButton.onclick = function () {
+  setUserName();
+};
 
 // END MOZILLA
 
@@ -49,7 +49,6 @@ const calculator = (() => {
   };
 })();
 
-console.log(calculator.add(3,5)); // 8
-console.log(calculator.sub(6,2)); // 4
-console.log(calculator.mul(14,5534)); // 77476
-
+console.log(calculator.add(3, 5)); // 8
+console.log(calculator.sub(6, 2)); // 4
+console.log(calculator.mul(14, 5534)); // 77476
--- javascript/webpack-demo/src/index.js
+++ javascript/webpack-demo/src/index.js
@@ -1,18 +1,18 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import _ from "lodash";
+import printMe from "./print.js";
 
 function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-    element.appendChild(btn);
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
