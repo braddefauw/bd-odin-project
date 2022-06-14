bcb2b3c5b091e025da54afdf18e0d64f262d2648
Brad DeFauw <badefauw@gmail.com>
2022-06-14 15:22:37 -0500

    zip code validity check added

--- javascript/scripts/main.js
+++ javascript/scripts/main.js
@@ -4,6 +4,8 @@ const form  = document.getElementsByTagName('form')[0];
 
 const email = document.getElementById('email');
 const emailError = document.querySelector('#email + span.error');
+const zipCode = document.getElementById('zip');
+const zipError = document.querySelector('#zip + span.error');
 
 email.addEventListener('input', function (event) {
   // Each time the user types something, we check if the
@@ -16,14 +18,29 @@ email.addEventListener('input', function (event) {
     emailError.className = 'error'; // Reset the visual state of the message
   } else {
     // If there is still an error, show the correct error
-    showError();
+    showEmailError();
+  }
+});
+
+zipCode.addEventListener('input', function (event) {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+
+  if (zipCode.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    zipCode.textContent = ''; // Reset the content of the message
+    zipError.className = 'error'; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showZipError();
   }
 });
 
 form.addEventListener('submit', function (event) {
   // if the email field is valid, we let the form submit
 
-  if(!email.validity.valid) {
+  if(!email.validity.valid || !zipCode.validity.valid) {
     // If it isn't, we display an appropriate error message
     showError();
     // Then we prevent the form from being sent by canceling the event
@@ -31,7 +48,21 @@ form.addEventListener('submit', function (event) {
   }
 });
 
-function showError() {
+function showZipError() {
+  if(zipCode.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    zipError.textContent = 'You need to enter a zip.';
+  } else if(zipCode.validity.patternMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    zipError.textContent = 'Please make sure to enter a valid zip code.';
+  }
+  // Set the styling appropriately
+  zipError.className = 'error active';
+}
+
+function showEmailError() {
   if(email.validity.valueMissing) {
     // If the field is empty,
     // display the following error message.
