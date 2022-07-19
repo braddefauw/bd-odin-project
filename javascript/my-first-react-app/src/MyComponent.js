a63c271890fdaca6dc0bea5b191f1fae1a1bfc6c
Brad DeFauw <badefauw@gmail.com>
2022-07-19 09:25:35 -0500

    TOP state and props practice

---
+++ javascript/my-first-react-app/src/MyComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { title, onButtonClicked } = this.props;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <button onClick={onButtonClicked}>Click Me!</button>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
+
+// functional components example
+
+// import React from 'react';
+
+// function MyComponent(props) {
+//   return <div>{props.title}</div>;
+// }
+
+// export default MyComponent;
